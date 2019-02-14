4a6792cd8bf93819fcd408319f747e243d455e3d
Danail Branekov <danailster@gmail.com>
2019-02-14 12:00:55 +0200

    Inline rootfs in repo
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- provision.sh
+++ provision.sh
@@ -2,27 +2,15 @@
 
 set -e
 
-BUSYBOX_VERSION=1.30.0
-
 apt update && apt upgrade --yes
-apt install --yes \
-  build-essential \
-  wget \
-  vim
+apt install --yes vim
 
 #install docker
 curl -sSL https://get.docker.com/ | sh
 
 # install busybox
-wget "https://busybox.net/downloads/busybox-${BUSYBOX_VERSION}.tar.bz2"
-tar -xjf busybox-1.30.0.tar.bz2
-
-pushd "busybox-${BUSYBOX_VERSION}"
-  cp /vagrant/.config .config
-  make && make install
-  mkdir /tmp/playground
-  cp -r _install /tmp/playground/rootfs
-popd
+mkdir /tmp/playground
+cp -r /vagrant/rootfs /tmp/playground/rootfs
 
 touch /I_AM_THE_HOST
 touch /tmp/playground/rootfs/I_AM_THE_CONTAINER
